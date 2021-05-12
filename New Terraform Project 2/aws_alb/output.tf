3ccbe701c812357f708b1fc1a698bb4b763d83ca
M93551 <79385241+M93551@users.noreply.github.com>
2021-05-11 19:23:21 -0700

    Create output.tf

---
+++ New Terraform Project 2/aws_alb/output.tf
@@ -0,0 +1,27 @@
+output "public_subnets" {
+  value = "${aws_subnet.public_subnet.*.id}"
+}
+
+output "security_group" {
+  value = "${aws_security_group.test_sg.id}"
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
+
+output "sub1" {
+  value = "${element(aws_subnet.public_subnet.*.id, 1 )}"
+}
+
+output "sub2" {
+  value = "${element(aws_subnet.public_subnet.*.id, 2 )}"
+}
+
+output "private_sub1" {
+  value = "${element(aws_subnet.private_subnet.*.id, 1 )}"
+}
+
+output "private_sub2" {
+  value = "${element(aws_subnet.private_subnet.*.id, 2 )}"
+}
