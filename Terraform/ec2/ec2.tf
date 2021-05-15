e370afe71f856149a093e3ef84de9f4d3b883aae
bestores <70006122+bestores@users.noreply.github.com>
2021-05-15 05:03:07 -0700

    Update ec2.tf

--- Terraform/ec2/ec2.tf
+++ Terraform/ec2/ec2.tf
@@ -1,1 +1,28 @@
+resource "aws_security_group" "sg_22" {
+  name = "sg_22"
+  vpc_id = aws_vpc.main.id
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
 
+
+
+resource "aws_instance" "proj2" {
+ami = "ami-0ca5c3bd5a268e7db "
+instance_type = "t2.micro"
+subnet_id = aws_subnet.subnet4.id
+associate_public_ip_address = "true"
+vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
+tags = { name = "proj2"}
+}
