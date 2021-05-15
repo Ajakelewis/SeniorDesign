20127be984a369170ccd89f6e456ded11d2e6aec
Ajakelewis <70390017+Ajakelewis@users.noreply.github.com>
2021-05-15 10:34:52 -0700

    so close

--- Terraform/iamvpc/route53.tf
+++ Terraform/iamvpc/route53.tf
@@ -1,16 +1,40 @@
 resource "aws_route53_zone" "blueteam-zone" {
    name = "blueteam481.com"
+}
+resource "aws_elb" "main" {
+  name               = "blueteam-terraform-elb"
+  availability_zones = ["us-west-2a"]
 
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
 }
 
-resource "aws_route53_record" "blueteam-record" {
-   zone_id = aws_route53_zone.blueteam-zone.zone_id
-   name = "blueteam481.com"
-   type = "A"
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.blueteam-zone.zone_id
+  name    = "blueteam481.com"
+  type    = "A"
 
- alias {
-   name = aws_lb.my-aws-alb.dns_name
-   zone_id = aws_lb.my-aws-alb.zone_id
-   evaluate_target_health = true
+  alias {
+    name                   = aws_elb.main.dns_name
+    zone_id                = aws_elb.main.zone_id
+    evaluate_target_health = true
   }
+}
+
+resource "aws_route53_record" "blueteam-zone" {
+  allow_overwrite = true
+  name            = "blueteam481.com"
+  ttl             = 172800
+  type            = "NS"
+  zone_id         = aws_route53_zone.blueteam-zone.zone_id
+  records = [
+    aws_route53_zone.blueteam-zone.name_servers[0],
+    aws_route53_zone.blueteam-zone.name_servers[1],
+    aws_route53_zone.blueteam-zone.name_servers[2],
+    aws_route53_zone.blueteam-zone.name_servers[3],
+  ]
 }
