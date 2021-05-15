8a69bb050240f4dfb8184606c700c8da63951fd8
Alex V <70228258+AV2302@users.noreply.github.com>
2021-05-14 18:09:15 -0700

    Update test.py
    Function is now able to: remove instance from ALB, check current spot price, and check what regions are running an instance.

--- Lambda/test.py
+++ Lambda/test.py
@@ -17,13 +17,12 @@ def lambda_handler(event, context):
     ''''instances variable holds the filtered results'''
     instances = ec2.instances.filter(Filters=filters)
     
-    '''response variable holds the results of the describe instances call'''
+    '''response variable holds the results of the describe instances call, such as instance id, region, etc
     response = client.describe_instances(Filters=filters)
-    print(response)
+    print(response)'''
     
     '''variable to hold the instance id of every runnning instance with the Purpose lambda tag'''
     RunningInstances = [instance.id for instance in instances]
-    print(RunningInstances)
     
     '''variable to hold the call to stop the instances then the variable is printed to run the calls
     shutDown = ec2.instances.filter(InstanceIds=RunningInstances).stop()
@@ -34,7 +33,6 @@ def lambda_handler(event, context):
     instanceID = instanceID.replace('[', '')
     instanceID = instanceID.replace(']', '')
     instanceID = instanceID.replace("'", '')
-    print(instanceID)  
     
     '''deregisters an instance from an application load balancer, instanceID is holding a string value of the instance id,
     this function can also register an instance by changing deregister_targets to register_targets, deregistering can take up to 5 minutes'''
@@ -47,3 +45,23 @@ def lambda_handler(event, context):
         ],
     )
     
+    prices=client.describe_spot_price_history(InstanceTypes=['t2.micro'],MaxResults=1,ProductDescriptions=['Linux/UNIX'],AvailabilityZone='us-west-2a')
+
+    print (prices['SpotPriceHistory'][0])
+    
+    prices=client.describe_spot_price_history(InstanceTypes=['t2.micro'],MaxResults=1,ProductDescriptions=['Linux/UNIX'],AvailabilityZone='us-west-2b')
+
+    print (prices['SpotPriceHistory'][0])
+    
+    prices=client.describe_spot_price_history(InstanceTypes=['t2.micro'],MaxResults=1,ProductDescriptions=['Linux/UNIX'],AvailabilityZone='us-west-2c')
+
+    print (prices['SpotPriceHistory'][0])
+    
+    regions=['us-east-1','us-east-2','us-west-1','us-west-2']
+    for i in regions:    
+        regionclient = boto3.client('ec2',region_name=i)
+        response = regionclient.describe_instances()
+        for reservation in response["Reservations"]:
+            for instance in reservation["Instances"]:
+                if instance['State']['Name'] == 'running':
+                    print(instance["InstanceId"], "is located in", instance["Placement"])
