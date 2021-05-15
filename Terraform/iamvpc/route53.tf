ca3d6bd7bec12c7905e73b5d412f58d78590fe51
bestores <70006122+bestores@users.noreply.github.com>
2021-05-15 05:07:41 -0700

    Update route53.tf

--- Terraform/iamvpc/route53.tf
+++ Terraform/iamvpc/route53.tf
@@ -1,16 +1,1 @@
-resource "aws_route53_zone" "blueteam-zone" {
-   name = "blueteam481.com"
 
-}
-
-resource "aws_route53_record" "blueteam-record" {
-   zone_id = aws_route53_zone.blueteam-zone.zone_id
-   name = "blueteam481.com"
-   type = "A"
-
- alias {
-   name = aws_lb.my-aws-alb.dns_name
-   zone_id = aws_lb.my-aws-alb.zone_id
-   evaluate_target_health = true
-  }
-}
